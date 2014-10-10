474de6c902c31241b87a61e08a164d6f2f99373d
szpaddy <sz_paddy@163.com>
2014-10-10 23:23:01 +0800

    add a few files

--- src/com/paddy/mynotes/ui/NoteEditActivity.java
+++ src/com/paddy/mynotes/ui/NoteEditActivity.java
@@ -1,9 +1,25 @@
 package com.paddy.mynotes.ui;
 
-public class NoteEditActivity {
+import android.app.Activity;
+import android.os.Bundle;
 
-	public NoteEditActivity() {
-		// TODO Auto-generated constructor stub
+import com.paddy.mynotes.R;
+import com.paddy.mynotes.editor.RichEditView;
+
+public class NoteEditActivity extends Activity {
+
+	private RichEditView noteEditView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.note_edit);
+
+		initResources();
+	}
+
+	private void initResources() {
+		noteEditView = (RichEditView) this.findViewById(R.id.noteEditView);
 	}
 
 }

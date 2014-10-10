31814b4ed2901246be6c4ef972092fca5b420b11
szpaddy <sz_paddy@163.com>
2014-10-10 15:25:25 +0800

    init project

---
+++ src/com/paddy/mynotes/ui/NoteEditActivity.java
@@ -0,0 +1,9 @@
+package com.paddy.mynotes.ui;
+
+public class NoteEditActivity {
+
+	public NoteEditActivity() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/paddy/mynotes/ui/NotesListActivity.java
@@ -0,0 +1,15 @@
+package com.paddy.mynotes.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotesListActivity extends Activity {
+
+	private static final String TAG = "NotesListActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}

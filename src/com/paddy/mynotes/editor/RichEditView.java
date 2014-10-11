092fee035c21337aacdc63e856d207973966449a
szpaddy <sz_paddy@163.com>
2014-10-11 18:00:50 +0800

    a few update, for purpose of sync data

--- src/com/paddy/mynotes/editor/RichEditView.java
+++ src/com/paddy/mynotes/editor/RichEditView.java
@@ -1,6 +1,11 @@
 package com.paddy.mynotes.editor;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.style.ImageSpan;
+import android.text.style.UnderlineSpan;
 import android.util.AttributeSet;
 import android.widget.EditText;
 
@@ -16,6 +21,25 @@ public class RichEditView extends EditText {
 
 	public RichEditView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
+	}
+
+	public static class BaseImageSpan extends ImageSpan {
+
+		public BaseImageSpan(Context context, int resourceId) {
+			super(context, resourceId);
+		}
+
+		@Override
+		public void draw(Canvas canvas, CharSequence text, int start, int end,
+				float x, int top, int y, int bottom, Paint paint) {
+			// TODO Auto-generated method stub
+			super.draw(canvas, text, start, end, x, top, y, bottom, paint);
+		}
+
+	}
+	
+	public static class MyUnderlineSpan extends UnderlineSpan{
+		
 	}
 
 }
--- src/com/paddy/mynotes/ui/NoteEditActivity.java
+++ src/com/paddy/mynotes/ui/NoteEditActivity.java
@@ -1,25 +1,73 @@
 package com.paddy.mynotes.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.paddy.mynotes.R;
 import com.paddy.mynotes.editor.RichEditView;
 
-public class NoteEditActivity extends Activity {
+public class NoteEditActivity extends Activity implements OnClickListener {
 
+	private ImageView mHeaderUp;
+	private TextView mHeaderDate;
+	private TextView mHeaderTime;
+	private ImageView mHeaderColor;
 	private RichEditView noteEditView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.note_edit);
+		this.getActionBar().setCustomView(R.layout.note_edit_header);
 
 		initResources();
 	}
 
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		//updateBackground();
+		//updateTimeHeader();
+	}
+
 	private void initResources() {
+		this.mHeaderUp = (ImageView) this.findViewById(R.id.headerUp);
+		this.mHeaderDate = (TextView) this.findViewById(R.id.headerDate);
+		this.mHeaderTime = (TextView) this.findViewById(R.id.headerTime);
+		this.mHeaderColor = (ImageView) this.findViewById(R.id.headerColor);
+		View[] headerViewAry = { mHeaderUp, mHeaderDate, mHeaderTime,
+				mHeaderColor };
+		for (View view : headerViewAry) {
+			view.setOnClickListener(this);
+		}
+
 		noteEditView = (RichEditView) this.findViewById(R.id.noteEditView);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.headerUp:
+			saveNote();
+			gotoNotesList();
+			this.finish();
+			break;
+		}
+	}
+
+	private void gotoNotesList() {
+		Intent intent = new Intent(this, NotesListActivity.class);
+		this.startActivity(intent);
+	}
+
+	private void saveNote() {
+
 	}
 
 }
